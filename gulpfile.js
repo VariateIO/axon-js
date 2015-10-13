1f5ee114fc41fb3c31602ea6b1fc7a936106bc0b
Gary McLean Hall <gary.hall@fourminutemile.co.uk>
2015-10-13 16:29:55 +0100

    Build copies files to output directory
    Version is included in cdn path.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,40 @@
 
 var gulp = require('gulp');
 var awspublish = require('gulp-awspublish');
+var git = require('gulp-git');
+var fs = require('fs');
+var path = require('path');
+var rename = require('gulp-rename');
+
+var bowerConfig = JSON.parse(fs.readFileSync('./bower.json'));
+var version = bowerConfig.version;
+
+var buildDir = './build/';
+var outDir = path.join(buildDir, version);
+
+var mkdirSync = function (path) {
+  try {
+    fs.mkdirSync(path);
+  } catch(e) {
+    if ( e.code != 'EEXIST' ) throw e;
+  }
+}
 
 gulp.task('default', [], function() {
 
+});
+
+gulp.task('build', [], function() {
+
+  mkdirSync(buildDir);
+  mkdirSync(outDir);
+  
+  gulp.src(bowerConfig.main)
+    .pipe(gulp.dest(outDir));
+});
+
+gulp.task('version', [], function() {
+  console.log(version);
 });
 
 gulp.task('publish-s3', [], function() {
@@ -15,8 +46,10 @@ gulp.task('publish-s3', [], function() {
     secretAccessKey: process.env.SECRET_ACCESS_KEY
   });
 
-  return gulp.src('variate.axon.js')
+  return gulp.src(outDir+'/**/*')
+    .pipe(rename(function (path) {
+      path.dirname += '/axon/'+version;
+    }))
     .pipe(publisher.publish())
     .pipe(awspublish.reporter());
 });
-

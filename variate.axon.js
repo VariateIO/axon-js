4a50714af0a141ab476cd2981768a51d4829924b
Gary Hall <info@variate.io>
2015-10-14 15:14:20 +0100

    Saving experiment value in session cookie

--- variate.axon.js
+++ variate.axon.js
@@ -4,13 +4,24 @@ if(!window.jQuery) {
 }
 
 $(function() {
-  $.ajax({
-    url: 'http://api.variate.io/'
-  }).done(function(data) {
+  var cookieName = 'variate-axon';
+  var assignment = $.cookie(cookieName);
+  if(!assignment) {
+    $.ajax({
+      url: 'http://api.variate.io/'
+    }).done(function(data) {
+      assignment = data;
+      $.cookie(cookieName, assignment);
+      onAssignment(assignment);
+    });
+  } else {
+    onAssignment(assignment);
+  }
+  function onAssignment(assignment) {
     var color = 'red';
-    if(data == 0) {
+    if(assignment == 0) {
       color = 'blue';
     }
     $('body').css('background-color', color);
-  });
+  }  
 });
